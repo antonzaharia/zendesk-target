eca320deabca8492a4b446a32c5c1dabb8a3c7d7
Anton Zaharia <zaharia_anton@yahoo.com>
2022-04-22 18:25:00 +0100

    print body

--- functions/zendeskTarget.js
+++ functions/zendeskTarget.js
@@ -1,11 +1,12 @@
 exports.handler = async function (event, context) {
   const { path, httpMethod, headers, queryStringParameters, body } = event
 
+  let details = await body.json()
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ event: event, context: context }),
+    body: JSON.stringify(details),
   }
 }
