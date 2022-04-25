069386453f7dd3a11dadce85888adce32f234697
Anton Zaharia <zaharia_anton@yahoo.com>
2022-04-25 09:20:13 +0100

    error fix

--- functions/zendeskTarget.js
+++ functions/zendeskTarget.js
@@ -1,12 +1,11 @@
 exports.handler = async function (event, context) {
   const { path, httpMethod, headers, queryStringParameters, body } = event
 
-  let details = await body.json()
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(details),
+    body: JSON.stringify(body),
   }
 }
