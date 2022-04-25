5506b989146c66875d27af92a47e0879f2ef7751
Anton Zaharia <zaharia_anton@yahoo.com>
2022-04-25 11:07:47 +0100

    parse json

--- functions/zendeskTarget.js
+++ functions/zendeskTarget.js
@@ -1,11 +1,13 @@
 exports.handler = async function (event, context) {
   const { path, httpMethod, headers, queryStringParameters, body } = event
 
+  let ticket = JSON.parse(body)['ticket']
+
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(body),
+    body: JSON.stringify(ticket),
   }
 }
