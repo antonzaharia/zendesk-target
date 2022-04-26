033fe185bb115cb9895357737f1d5d63b0e1b1f8
Anton Zaharia <zaharia_anton@yahoo.com>
2022-04-26 10:42:45 +0100

    changed to queue

--- functions/zendeskTarget.js
+++ functions/zendeskTarget.js
@@ -1,13 +1,23 @@
 exports.handler = async function (event, context) {
-  const { path, httpMethod, headers, queryStringParameters, body } = event
-
-  let ticket = JSON.parse(body)['ticket']
-
+  let data = { text: 'Bigger than five' }
+  if (true) {
+    fetch('https://hooks.slack.com/services/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', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data)
+      })
+  }
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(ticket),
+    body: JSON.stringify(data),
   }
 }
