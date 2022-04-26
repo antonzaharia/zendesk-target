95edde7a07d63910b8ea372053de4fcc19afdc5d
Anton Zaharia <zaharia_anton@yahoo.com>
2022-04-26 10:47:19 +0100

    install fetch

--- functions/zendeskTarget.js
+++ functions/zendeskTarget.js
@@ -1,5 +1,7 @@
+import fetch from 'node-fetch'
 exports.handler = async function (event, context) {
   let data = { text: 'Bigger than five' }
+
   if (true) {
     fetch('https://hooks.slack.com/services/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', {
       method: 'POST',
